44caa79f22876b3be5ad2343df5c1a8bb1f1b57e
David Kellerman <david_kellerman@nls.com>
2016-08-03 15:02:37 -0700

    Changed calling sequences for better optional parameter handling.
    The uploadFile() bucketName paramter always had to be passed, and
    setting it in the config has no effect.
    
    Changed uploadFile() to remove the bucketName parameter. It uses the
    config setting by default, and the caller can override the default by
    specifying a `Bucket` setting in the `s3Options` parameter.
    
    Changed getUploadURL() to move all parameters into an options parameter.

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ import Promise from 'bluebird'
  * 
  * ## Server side uploads
  * 
- * In your module, reqest `app.get('s3-files').uploadFile(bucketName, fileName, contents)` to send a file to the specified bucket on S3.
+ * In your module, reqest `app.get('s3-files').uploadFile(fileName, contents, {Bucket: bucketName})` to send a file to the specified bucket on S3.
  * 
  * 
  * 
@@ -63,51 +63,59 @@ export default class S3Files {
   }
 
   /**
-   * Request a URL for uploading directly to an S3 bucket
-   * @param {String} bucketName S3 Bucket to store files
-   * @param {String} [directURL] The URL to register for signing uploads
-   * @param {string} [includeScript] Template name that should include the helper script
-   * @param {boolean} [adminOnly] Should the URL require an admin user?
+   * Request a URL for uploading directly to an S3 bucket.
+   * @param {object} options (optional) Upload configuration options:
+   *   bucketName, directURL, includeScript and adminOnly.
+   *   Specify bucketName to select the S3 Bucket for the upload,
+   *   overriding the configuration setting. Specify directURL to define
+   *   the route for signing the upload request, overriding the
+   *   configuration setting. Specify includeScript to select a
+   *   template name that should include the help script. Set adminOnly
+   *   to true to require an admin user for the URL (default), or false
+   *   to not require an admin user.
    * @returns {object} url: and js: keys.
    */
-  getUploadURL(bucketName, directURL, includeScript=false, adminOnly=true) {
-    if (directURL === undefined) {
-      directURL = this.config.directURL
-    }
-    this.app.get('router').route('GET', directURL, _.bind(this._directURLHandler, this, bucketName))
-    if (adminOnly) {
-      this.app.get('users').ensureAdmin(directURL)
+  getUploadURL(options) {
+    let config = Object.assign({
+      includeScript: false,
+      adminOnly: true
+    }, this.config, options)
+    this.app.get('router').route('GET', config.directURL, _.bind(this._directURLHandler, this, config.bucketName))
+    if (config.adminOnly) {
+      this.app.get('users').ensureAdmin(config.directURL)
     }
     let jsURL = this.config.directURL+"/js/s3direct.js"
-    if (includeScript) {
-      this.app.get('templater').on('renderContext.'+includeScript, () => {
+      // TO DO: is this right? (always uses configured directURL)
+    if (config.includeScript) {
+      this.app.get('templater').on('renderContext.'+config.includeScript, () => {
         return {scripts: [jsURL]}
       })
     }
 
-    return {url: directURL, js: jsURL}
+    return {url: config.directURL, js: jsURL}
   }
 
   /**
-   * Upload a file to S3
-   * @param {String} bucketName S3 Bucket to store files
-   * @param {string} filename Name of file on S3
-   * @param {String|Buffer} contents The file contents
-   * @param {object} [s3Options] Additional parameters for S3 putObject
-   * @returns {String} URL of uploaded file
+   * Upload a file to S3.
+   * @param {string} filename Name of file on S3.
+   * @param {String|Buffer} contents The file contents.
+   * @param {object} [s3Options] Additional parameters for S3 putObject.
+   *   For example, you can specify a `Bucket` parameter to select the
+   *   AWS Bucket for the upload, overriding the configuration setting.
+   * @returns {String} URL of uploaded file.
    */
-  uploadFile(bucketName, filename, contents, s3Options={}) {
+  uploadFile(filename, contents, s3Options={}) {
     var s3 = new aws.S3()
     Promise.promisifyAll(Object.getPrototypeOf(s3))
     var s3Params = Object.assign({
-      Bucket: bucketName,
+      Bucket: this.config.bucketName,
       Key: filename,
       Body: contents,
       Expires: 60,
       ACL: 'public-read'
     }, s3Options)
     return s3.putObjectAsync(s3Params).then((data) => {
-      return this.fileURL(bucketName, filename)
+      return this.fileURL(s3Params.Bucket, s3Params.Key)
     })
   }
 
